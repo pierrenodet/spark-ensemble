1bca2e10b18b4d306b77c8c6e0154a556d1cddd1
Pierre Nodet <nodet.pierre@gmail.com>
2019-06-27 17:08:38 +0200

    Update BaggingParams.scala

--- src/main/scala/org/apache/spark/ml/bagging/BaggingParams.scala
+++ src/main/scala/org/apache/spark/ml/bagging/BaggingParams.scala
@@ -10,4 +10,8 @@ trait BaggingParams
     with HasParallelism
     with HasWeightCol
     with HasBaseLearner
-    with SubSpaceParams {}
+    with SubSpaceParams {
+
+  setDefault(maxIter -> 10)
+        
+}
