c4975942f69c150d5a3ab7d9722be591626ef919
pierrenodet <nodet.pierre@gmail.com>
2019-08-20 23:33:04 +0200

    fix travis and footer of microsite

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -21,7 +21,7 @@ class Footer extends React.Component {
           <section className="copyright">
             {this.props.config.copyright}
              <br />
-             Icon made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+             Icon made by <a href="https://www.flaticon.com/authors/photo3idea-studio" title="photo3idea_studio">photo3idea_studio</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
           </section>
         </footer>
       );
